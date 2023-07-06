986a6a07389b9377a784ab1937ee42e5e7668930
Elinor <elinorco24@gmail.com>
2023-07-06 17:35:40 +0300

    Edit login page design #1
    add icons to the form, add the logo, create animated gradient background, change the login button

--- client/src/pages/Login/Login.style.js
+++ client/src/pages/Login/Login.style.js
@@ -1,5 +1,17 @@
-import styled from "styled-components";
+import styled,{ keyframes } from "styled-components";
 import { Link } from "react-router-dom";
+
+const HeaderKeyFrame = keyframes`
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+`;
 
 export const Wrapper = styled.div`
   height: 100vh;
@@ -7,7 +19,11 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: linear-gradient(100deg, #673ab7, #934545);
+  background-image: linear-gradient(      217deg,      rgba(0,255,159, 0.8),      rgb(165 43 43 / 0%) 70.71%    ), linear-gradient(127deg, rgba(0,184,255, 0.8), rgba(0,30,255, 0) 70.71%),    linear-gradient(358deg, rgba(189,0,255, 0.8), rgba(214,0,255, 0) 70.71%);
+  background-position: top center;
+  animation: ${HeaderKeyFrame} 4500ms ease infinite;
+  background-size: 200% 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
 `;
 
 export const Form = styled.form`
@@ -23,27 +39,28 @@ export const Form = styled.form`
 `;
 
 export const SubmitButton = styled.button`
-  background-image: linear-gradient(100deg, #673ab7, #934545);
-  color: white;
+  background-color: #e4e4e4;
+  color: black;
   border-radius: 30px;
-  font-size: 1.1rem;
+  font-size: 1.3rem;
   padding-block: 1rem;
   font-weight: 600;
   border: none;
   transition: all 200ms linear;
   &:hover {
-    scale: 1.1;
+    scale: 1.05;
     cursor: pointer;
-    box-shadow: 5px 3px 7px grey;
+    box-shadow: 0px 0px 9px grey;
   }
 `;
 
 export const FieldWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
 `;
 
 export const Input = styled.input`
+  width: 100%;
   padding: 4px;
   border: none;
   border-bottom: 1px solid grey;
@@ -53,14 +70,21 @@ export const Input = styled.input`
   }
 `;
 
-export const Title = styled.h1`
+export const Title = styled.img`
   margin-block: 0;
-  text-align: center;
-  font-size: 2rem;
-  line-height: 2.5rem;
-  letter-spacing: -1px;
+`;
+
+export const Icon = styled.img`
+padding: 3px;
+height: 18px;
+width: 18px;
+border-inline-color: #e4e4e4;
+padding-right: 5px;
+border-bottom: 1px solid grey;
 `;
 
 export const ForgotPassword = styled(Link)`
   align-self: flex-end;
 `;
+
+
