2841bd1f097620f45ec9332b85abb50534e19956
Shon Khundiashvili <shonk2603@gmail.com>
2023-07-09 10:35:18 +0300

    added a new back button

--- client/src/pages/ForgotPassword/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,5 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import {
+  Input,
+  Form,
+  Wrapper,
+  ResetPasswordButton,
+  Alignp,
+  AlignH1,
+  BackButton,
+} from "./ForgotPassword.style";
+
 function ForgotPassword() {
-  return <h1>Forgot password?</h1>;
+  const navigate = useNavigate();
+  function handleSubmit(event) {
+    event.preventDefault();
+    const { target } = event;
+    const formData = new FormData(target);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+  }
+
+  function returnHomePage() {
+    navigate("/login");
+  }
+
+  return (
+    <Wrapper>
+      <Form onSubmit={handleSubmit}>
+        <AlignH1>Forgot Password?</AlignH1>
+        <Alignp>Reset password in one quick step</Alignp>
+        <Input name="Email" type="email" placeholder="Enter your email" />
+        <ResetPasswordButton>Reset Password</ResetPasswordButton>
+        <BackButton onClick={returnHomePage}>Back</BackButton>
+      </Form>
+    </Wrapper>
+  );
 }
 
 export default ForgotPassword;
